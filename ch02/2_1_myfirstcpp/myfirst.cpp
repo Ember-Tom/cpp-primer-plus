27afbbfaa891c5d77aa88de99f9d473b660fd3f6
Ember <YJEmber@outlook.com>
2025-10-05 23:56:43 +0800

    Add 'myfirst.cpp' code 2025/10/05

---
+++ ch02/2_1_myfirstcpp/myfirst.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const **argv)
+{
+    cout << "Hello World!";
+    cout << endl;
+    cout << "This is my first cpp code!" << endl;
+    return EXIT_SUCCESS;
+}
